8e8b55341eeead3baaf55029abd05b8875ea320b
Yvonne Allen <yallen011@gmail.com>
2018-06-01 20:20:02 -0400

    fixed broken text. needed to wrap inside a MaterialApp widget because text widget requires to be wrapped in Directionailty widget and MaterialApp widget does that.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,30 @@
+import 'package:flutter/material.dart';
 // This is a basic Flutter widget test.
 // To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:hello_rectangle/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Text is displayed smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new HelloRectangle());
+    await tester.pumpWidget(new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(
+            'Hello!',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+        body: new HelloRectangle(),
+      ),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that Hello! is displayed
+    expect(find.text('Hello!'), findsNWidgets(2));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
